595a1b22a103a6d56982552a1f7562ef217b49f8
gift56 <efeasiughu@gmail.com>
2023-04-20 14:16:00 -0700

    testing endpoint out

--- client/src/components/AIPicker.jsx
+++ client/src/components/AIPicker.jsx
@@ -1,9 +1,40 @@
 import React from "react";
 import Button from "./Button";
 
-
 const AIPicker = ({ prompt, setPrompt, generatingImg, handleSubmit }) => {
-  return <div>AIPicker</div>;
+  return (
+    <div className="aipicker-container">
+      <textarea
+        placeholder="Ask AI..."
+        rows={5}
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+        className="aipicker-textarea resize-none"
+      />
+      <div className="flex flex-wrap gap-3">
+        {generatingImg ? (
+          <Button
+            text="Asking AI..."
+            className="text-xs  border border-[#EFBD48]"
+          />
+        ) : (
+          <>
+            <Button
+              text="AI Logo"
+              handleClick={() => handleSubmit("logo")}
+              className="text-xs  border border-[#EFBD48]"
+            />
+
+            <Button
+              text="AI Full"
+              handleClick={() => handleSubmit("full")}
+              className="text-xs  bg-[#EFBD48] text-white"
+            />
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default AIPicker;
