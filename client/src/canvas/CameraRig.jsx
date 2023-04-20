644be27f6c34d1c40c85f476e74e19bcd1b0cdc9
gift56 <efeasiughu@gmail.com>
2023-04-20 06:07:00 -0700

    working on camera rotation

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -14,16 +14,16 @@ const CameraRig = ({ children }) => {
 
     // set the initial position of the model
     let targetPosition = [-0.4, 0, 2];
-    if(snap.intro) {
-      if(isBreakpoint) targetPosition = [0, 0, 2];
-      if(isMobile) targetPosition = [0, 0.2, 2.5];
+    if (snap.intro) {
+      if (isBreakpoint) targetPosition = [0, 0, 2];
+      if (isMobile) targetPosition = [0, 0.2, 2.5];
     } else {
-      if(isMobile) targetPosition = [0, 0, 2.5]
+      if (isMobile) targetPosition = [0, 0, 2.5];
       else targetPosition = [0, 0, 2];
     }
 
     // set model camera position
-    easing.damp3(state.camera.position, targetPosition, 0.25, delta)
+    easing.damp3(state.camera.position, targetPosition, 0.25, delta);
 
     // set the model rotation smoothly
     easing.dampE(
@@ -31,8 +31,8 @@ const CameraRig = ({ children }) => {
       [state.pointer.y / 10, -state.pointer.x / 5, 0],
       0.25,
       delta
-    )
-  })
+    );
+  });
 
   return <group ref={group}>{children}</group>;
 };
