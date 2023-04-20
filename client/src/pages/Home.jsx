90b09db22b8731823f60b4ff127d8afc9ad10a12
gift56 <efeasiughu@gmail.com>
2023-04-20 05:02:40 -0700

    start coding

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -11,8 +11,36 @@ import state from "../store";
 
 const Home = () => {
   const snap = useSnapshot(state);
-  
-  return <div>Home</div>;
+
+  return (
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className="home" {...slideAnimation("left")}>
+          <motion.header {...slideAnimation("down")}>
+            <img
+              src="./threejs.png"
+              alt="logo"
+              className="w-8 h-8 object-contain"
+            />
+          </motion.header>
+          <motion.div {...headContentAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className="head-text uppercase">
+                Let's <br className="xl:block hidden" /> do it.
+              </h1>
+            </motion.div>
+            <motion.div {...headContentAnimation} className="flex flex-col gap-5">
+              <p className="max-w-md font-normal text-gray-600 text-base">
+                Create your unique and exclusive shirt with our brand-new 3D
+                customization tool. <strong>Unleash your imagination</strong>{" "}
+                and define your own style.
+              </p>
+            </motion.div>
+          </motion.div>
+        </motion.section>
+      )}
+    </AnimatePresence>
+  );
 };
 
 export default Home;
