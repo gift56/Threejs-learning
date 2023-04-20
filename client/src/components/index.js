107890315ad8a1069f68807ef21969c099e4d8f6
gift56 <efeasiughu@gmail.com>
2023-04-20 05:22:05 -0700

    added new components

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,7 @@
 import Button from "./Button";
+import Tab from "./Tab";
+import FilePicker from "./FilePicker";
+import AIPicker from "./AIPicker";
+import ColorPicker from "./ColorPicker";
 
-export { Button };
+export { Button, AIPicker, FilePicker, ColorPicker, Tab };
