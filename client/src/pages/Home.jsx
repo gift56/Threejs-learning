b15e0a769236848cf7143ec7e69744acb241b70e
gift56 <efeasiughu@gmail.com>
2023-04-20 05:14:08 -0700

    done with home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,6 +8,7 @@ import {
   slideAnimation,
 } from "../config/motion";
 import state from "../store";
+import { Button } from "../components";
 
 const Home = () => {
   const snap = useSnapshot(state);
@@ -29,12 +30,21 @@ const Home = () => {
                 Let's <br className="xl:block hidden" /> do it.
               </h1>
             </motion.div>
-            <motion.div {...headContentAnimation} className="flex flex-col gap-5">
+            <motion.div
+              {...headContentAnimation}
+              className="flex flex-col gap-5"
+            >
               <p className="max-w-md font-normal text-gray-600 text-base">
                 Create your unique and exclusive shirt with our brand-new 3D
                 customization tool. <strong>Unleash your imagination</strong>{" "}
                 and define your own style.
               </p>
+              <Button
+                text="Customize It"
+                type="filled"
+                handleClick={() => (state.intro = false)}
+                className="w-fit px-4 py-2.5 font-bold text-sm bg-[#EFBD48] text-white"
+              />
             </motion.div>
           </motion.div>
         </motion.section>
