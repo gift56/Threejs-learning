ec2c77e069635113301c12a8bcc517ac6ca75cab
gift56 <efeasiughu@gmail.com>
2023-04-20 06:08:50 -0700

    working on camera rotation

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -23,7 +23,28 @@ const Shirt = () => {
         material={materials.lambert1}
         material-roughness={1}
         dispose={null}
-      ></mesh>
+      >
+        {snap.isFullTexture && (
+          <Decal
+            position={[0, 0, 0]}
+            rotation={[0, 0, 0]}
+            scale={1}
+            map={fullTexture}
+          />
+        )}
+
+        {snap.isLogoTexture && (
+          <Decal
+            position={[0, 0.04, 0.15]}
+            rotation={[0, 0, 0]}
+            scale={0.15}
+            map={logoTexture}
+            map-anisotropy={16}
+            depthTest={false}
+            depthWrite={true}
+          />
+        )}
+      </mesh>
     </group>
   );
 };
