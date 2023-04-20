73b6e3a1f3b47e562e236045affad9b6252102d7
gift56 <efeasiughu@gmail.com>
2023-04-20 05:32:58 -0700

    added new components

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -7,9 +7,41 @@ import { download } from "../assets";
 import { downloadCanvasToImage, reader } from "../config/helpers";
 import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants";
 import { fadeAnimation, slideAnimation } from "../config/motion";
+import { AIPicker, ColorPicker, Button, Tab, FilePicker } from "../components";
 
 const Customizer = () => {
-  return <div>Customizer</div>;
+  const snap = useSnapshot(state);
+  return (
+    <AnimatePresence>
+      {!snap.intro && (
+        <>
+          <motion.div
+            key="custom"
+            className="absolute top-0 left-0 z-10"
+            {...slideAnimation("left")}
+          >
+            <div className="flex min-h-screen items-center">
+              <div className="tabs editortabs-container">
+                {EditorTabs.map((tab) => (
+                  <Tab key={tab.name} tab={tab} handleClick={() => {}} />
+                ))}
+              </div>
+            </div>
+          </motion.div>
+          <motion.div
+            className="absolute z-10 top-5 right-5"
+            {...fadeAnimation}
+          >
+            <Button
+              text="Go Back"
+              handleClick={() => (state.intro = true)}
+              className="w-fit px-4 py-2.5 font-bold text-sm bg-[#EFBD48] text-white"
+            />
+          </motion.div>
+        </>
+      )}
+    </AnimatePresence>
+  );
 };
 
 export default Customizer;
