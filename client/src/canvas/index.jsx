0ddfd24cb2c8a38c9a28ad4aa7840a748f9a103c
gift56 <efeasiughu@gmail.com>
2023-04-20 05:51:23 -0700

    working on canvas

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { Environment, Center } from "@react-three/drei";
+import Shirt from "./Shirt";
+import CameraRig from "./CameraRig";
+import Backdrop from "./BackDrop";
 
 const CanvasModel = () => {
   return <div>Canvas</div>;
