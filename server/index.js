998bca6367c399ac76cf597e8a1840312c6fbb38
gift56 <efeasiughu@gmail.com>
2023-04-20 13:58:19 -0700

    working on sever

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
+import dalleRoutes from './routes/dalle.routes.js';
 
 dotenv.config();
 
@@ -8,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limig: "50mb" }));
 
-// app.use("/api/v1/dalle", dalleRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello from DALL.E Server" });
