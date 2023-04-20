342155df60b49a29dab646a10f21c3720309fc08
gift56 <efeasiughu@gmail.com>
2023-04-20 06:17:08 -0700

    working on back drop

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -7,7 +7,12 @@ import Backdrop from "./BackDrop";
 
 const CanvasModel = () => {
   return (
-    <Canvas>
+    <Canvas
+      shadows
+      camera={{ position: [0, 0, 0], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+      className="w-full max-w-full h-full transition-all ease-in"
+    >
       <ambientLight intensity={0.5} />
       <Environment preset="city" />
       <CameraRig>
