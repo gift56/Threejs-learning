8893f3f7e771547fc73330a1039ef804440d92c6
gift56 <efeasiughu@gmail.com>
2023-04-20 07:46:06 -0700

    done with with filepicker

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -41,6 +41,32 @@ const Customizer = () => {
     }
   };
 
+  const handleSubmit = async (type) => {
+    if (!prompt) return alert("Please enter a prompt");
+
+    try {
+      setGeneratingImg(true);
+      const response = await fetch("http://localhost:8080/api/v1/dalle", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          prompt,
+        }),
+      });
+
+      const data = await response.json();
+
+      handleDecals(type, `data:image/png;base64,${data.photo}`);
+    } catch (error) {
+      alert(error);
+    } finally {
+      setGeneratingImg(false);
+      setActiveEditorTab("");
+    }
+  };
+
   const handleDecals = (type, result) => {
     const decalType = DecalTypes[type];
 
