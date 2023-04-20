84a681ebd88b0ae6edeac84ebc97e1b63fd1e5be
gift56 <efeasiughu@gmail.com>
2023-04-20 05:17:31 -0700

    working on customize page

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { useSnapshot } from "valtio";
+import config from "../config/config";
+import state from "../store";
 
 const Customizer = () => {
-  return (
-    <div>Customizer</div>
-  )
-}
+  return <div>Customizer</div>;
+};
 
-export default Customizer
+export default Customizer;
