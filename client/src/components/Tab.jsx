05f7fad9e585e20655e0375c0f9e119dda252aae
gift56 <efeasiughu@gmail.com>
2023-04-20 06:33:24 -0700

    working on tabs

--- client/src/components/Tab.jsx
+++ client/src/components/Tab.jsx
@@ -8,7 +8,25 @@ const Tab = ({ tab, isFilterTab, isActiveTab, handleClick }) => {
     isFilterTab && isActiveTab
       ? { backgroundColor: snap.color, opacity: 0.5 }
       : { backgroundColor: "transparent", opacity: 1 };
-  return <div>Tab</div>;
+
+  return (
+    <div
+      key={tab.name}
+      className={`tab-btn ${
+        isFilterTab ? "rounded-full glassmorphism" : "rounded-4"
+      }`}
+      onClick={handleClick}
+      style={activeStyles}
+    >
+      <img
+        src={tab.icon}
+        alt={tab.name}
+        className={`${
+          isFilterTab ? "w-2/3 h-2/3" : "w-11/12 h-11/12 object-contain"
+        }`}
+      />
+    </div>
+  );
 };
 
 export default Tab;
