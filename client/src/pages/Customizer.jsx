403eaf54053cd648cc38bc72f3e00e091f7443e3
gift56 <efeasiughu@gmail.com>
2023-04-20 05:19:25 -0700

    working on customize page

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -5,6 +5,8 @@ import config from "../config/config";
 import state from "../store";
 import { download } from "../assets";
 import { downloadCanvasToImage, reader } from "../config/helpers";
+import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants";
+import { fadeAnimation, slideAnimation } from "../config/motion";
 
 const Customizer = () => {
   return <div>Customizer</div>;
