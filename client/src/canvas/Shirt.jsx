83162c6421dc65bd745b8fa89acffc514fef3b3c
gift56 <efeasiughu@gmail.com>
2023-04-20 05:59:07 -0700

    working on canvas

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -1,11 +1,31 @@
 import React from "react";
-import { easing } from 'maath';
-import { useSnapshot } from 'valtio';
-import { useFrame } from '@react-three/fiber';
-import { Decal, useGLTF, useTexture } from '@react-three/drei';
+import { easing } from "maath";
+import { useSnapshot } from "valtio";
+import { useFrame } from "@react-three/fiber";
+import { Decal, useGLTF, useTexture } from "@react-three/drei";
+import state from "../store";
 
 const Shirt = () => {
-  return <div>Shirt</div>;
+  const snap = useSnapshot(state);
+  const { nodes, materials } = useGLTF("/shirt_baked.glb");
+  const logoTexture = useTexture(snap.logoDecal);
+  const fullTexture = useTexture(snap.fullDecal);
+  useFrame((state, delta) =>
+    easing.dampC(materials.lambert1.color, snap.color, 0.25, delta)
+  );
+  const stateString = JSON.stringify(snap);
+
+  return (
+    <group>
+      <mesh
+        castShadow
+        geometry={nodes.T_Shirt_male.geometry}
+        material={materials.lambert1}
+        material-roughness={1}
+        dispose={null}
+      ></mesh>
+    </group>
+  );
 };
 
 export default Shirt;
