7aa3dfd8fc6153827bbcad6e5b1ca85d1fb344a6
gift56 <efeasiughu@gmail.com>
2023-04-20 06:23:05 -0700

    done working on canvas

--- client/src/components/Tab.jsx
+++ client/src/components/Tab.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
+import { useSnapshot } from 'valtio'
+import state from '../store';
 
 const Tab = () => {
-  return (
-    <div>Tab</div>
-  )
-}
+  return <div>Tab</div>;
+};
 
-export default Tab
+export default Tab;
